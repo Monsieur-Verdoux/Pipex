72379fbd52a5dc3024aafa9dd77efaab0969e7e2
Andrey Kovalevskiy <akovalev@student.hive.fi>
2024-02-09 17:01:13 +0200

    norm compliant, needs further work on error checking

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: akovalev <akovalev@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/05 17:29:21 by akovalev          #+#    #+#             */
-/*   Updated: 2024/02/08 18:45:48 by akovalev         ###   ########.fr       */
+/*   Updated: 2024/02/09 17:00:42 by akovalev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,6 @@ void	free_split(char **arr)
 	int	i;
 
 	i = 0;
-
 	while (arr[i])
 	{
 		free(arr[i]);
@@ -66,7 +65,6 @@ char	*check_command(t_pipex *p, int index)
 		p->com_params_1 = ft_split(p->argv[index], ' ');
 	else if (index == 3)
 		p->com_params_2 = ft_split(p->argv[index], ' ');
-
 	i = 0;
 	while (p->paths[i])
 	{
@@ -85,36 +83,6 @@ char	*check_command(t_pipex *p, int index)
 	return (NULL);
 }
 
-int	validate_arguments(t_pipex *p)
-{
-	p->cmd1 = check_command(p, 2);
-	if (!p->cmd1)
-	{
-		perror("pipex");
-		return (0);
-	}
-	p->cmd2 = check_command(p, 3);
-	if (!p->cmd2)
-	{
-		perror("pipex");
-		return (0);
-	}
-	p->input = open(p->argv[1], O_RDONLY);
-	if (p->input == -1)
-	{
-		perror("Failure opening file");
-		return (0);
-	}
-	if (access(p->argv[4], F_OK) != -1)
-	{
-		if (access(p->argv[4], W_OK) != 0)
-		{
-			perror("");
-			return (0);
-		}
-	}
-	return (1);
-}
 void	initialize_struct(t_pipex *p, int argc, char **argv, char **env)
 {
 	p->argc = argc;
@@ -126,16 +94,32 @@ void	initialize_struct(t_pipex *p, int argc, char **argv, char **env)
 	p->com_params_2 = NULL;
 	p->paths = NULL;
 }
+
 void	first_child(t_pipex *p)
 {
 	close(p->pipefd[0]);
+	p->input = open(p->argv[1], O_RDONLY);
+	if (p->input == -1)
+	{
+		perror("Failure opening file");
+		free_all(p);
+		exit (EXIT_FAILURE);
+	}
+	p->cmd1 = check_command(p, 2);
+	if (!p->cmd1)
+	{
+		perror("pipex");
+		free_all(p);
+		exit (EXIT_FAILURE);
+	}
 	dup2(p->pipefd[1], STDOUT_FILENO);
 	dup2(p->input, STDIN_FILENO);
 	close(p->pipefd[1]);
 	close(p->input);
 	execve(p->cmd1, p->com_params_1, p->env);
 	perror("execve");
-	//exit (EXIT_FAILURE);
+	free_all(p);
+	exit (EXIT_FAILURE);
 }
 
 void	second_child(t_pipex *p)
@@ -143,14 +127,68 @@ void	second_child(t_pipex *p)
 	close(p->pipefd[1]);
 	p->output = open(p->argv[4], O_WRONLY | O_CREAT | O_TRUNC, 0666);
 	if (p->output == -1)
-		perror("open");
+	{
+		perror("Failure opening file");
+		free_all(p);
+		exit (EXIT_FAILURE);
+	}
+	p->cmd2 = check_command(p, 3);
+	if (!p->cmd2)
+	{
+		perror("pipex");
+		free_all(p);
+		exit (EXIT_FAILURE);
+	}
 	dup2(p->pipefd[0], STDIN_FILENO);
 	dup2(p->output, STDOUT_FILENO);
 	close(p->pipefd[0]);
 	close(p->output);
 	execve(p->cmd2, p->com_params_2, p->env);
 	perror("execve");
-	//exit (EXIT_FAILURE);
+	free_all(p);
+	exit (EXIT_FAILURE);
+}
+
+void	initial_checks(t_pipex *p, int argc, char **argv, char **env)
+{
+	if (argc != 5)
+	{
+		ft_printf("Please provide exactly four arguments\n");
+		exit (EXIT_FAILURE);
+	}
+	initialize_struct(p, argc, argv, env);
+	p->paths = parse_paths(env);
+	if (p->paths == NULL)
+	{
+		perror("path");
+		exit(EXIT_FAILURE);
+	}
+	if (pipe(p->pipefd) == -1)
+	{
+		perror("pipe");
+		exit(EXIT_FAILURE);
+	}
+}
+
+void	forking(t_pipex *p)
+{
+	p->pid = fork();
+	if (p->pid < 0)
+	{
+		perror("fork");
+		exit(EXIT_FAILURE);
+	}
+	if (p->pid == 0)
+		first_child(p);
+	p->pid2 = fork();
+	if (p->pid2 < 0)
+	{
+		perror("fork");
+		free_all(p);
+		exit(EXIT_FAILURE);
+	}
+	if (p->pid2 == 0)
+		second_child(p);
 }
 
 int	main(int argc, char **argv, char**env)
@@ -158,51 +196,22 @@ int	main(int argc, char **argv, char**env)
 	t_pipex	p;
 	int		status;
 
-	if (argc != 5)
-	{
-		ft_printf("There should be 4 arguments\n");
-		exit (EXIT_FAILURE);
-	}
-	initialize_struct(&p, argc, argv, env);
-	p.paths = parse_paths(env);
-	if (p.paths == NULL)
-		exit(EXIT_FAILURE);
-	if (!validate_arguments(&p))
-	{
-		free_all(&p);
-		return (0);
-	}
-	if (pipe(p.pipefd) == -1)
-		perror("pipe");
-	p.pid = fork();
-	if (p.pid < 0)
-	{
-		perror("fork");
-		return (0);
-	}
-	if (p.pid == 0)
-		first_child(&p);
-	p.pid2 = fork();
-	if (p.pid2 < 0)
-	{
-		perror("fork");
-		return (0);
-	}
-	if (p.pid2 == 0)
-		second_child(&p);
+	initial_checks(&p, argc, argv, env);
+	forking (&p);
 	close(p.pipefd[0]);
 	close(p.pipefd[1]);
 	if (waitpid(p.pid, &status, 0) == -1)
 	{
 		perror("waitpid");
-		//exit(EXIT_FAILURE);
+		free_all(&p);
+		exit(EXIT_FAILURE);
 	}
 	if (waitpid(p.pid2, &status, 0) == -1)
 	{
 		perror("waitpid");
-		//exit(EXIT_FAILURE);
+		free_all(&p);
+		exit(EXIT_FAILURE);
 	}
 	free_all(&p);
 	return (WEXITSTATUS(status));
 }
-
